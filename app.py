31aa88671868e1ef421e1a3c2290b175b03ba04c
t-rkr <tarunrulz11@gmail.com>
2020-03-15 20:57:46 +0530

    Bunch of changes

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from flask import Flask,jsonify, render_template, redirect, request, url_for
 
+
 app = Flask(__name__)
 
+
+@app.route('/chatbot')
+def chatbot():
+    return render_template('tabs/chatbot.html')
+
+@app.route('/timeline')
+def timeline():
+    return render_template('tabs/timeline.html')
+
+@app.route('/tweets')
+def tweets():
+    return render_template('tabs/tweets.html')
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-
+#
+# @app.route('/send_message', methods=['POST'])
+# def chatbot_message():
+#     message = str(request.get_data(),'utf-8').split("=")[1]
+#     chat_response = chatbot.chatbot_response(message)
+#     return "Okay cool"
+#
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=9001)
