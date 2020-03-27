93833a4201d7e0c3f15d388849ec411056d4b5f4
t-rkr <tarunrulz11@gmail.com>
2020-03-27 22:01:48 +0530

    Changed timeline graphs

--- app.py
+++ app.py
@@ -12,93 +12,66 @@ def chatbot():
 @app.route('/chat',methods=["POST"])
 def chat():
     message = request.form['message']
-    print(message)
-    url = "http://127.0.0.1:5000/chat"
     querystring = {"message": message}
-    reply = requests.request("POST",url,params=querystring).json()
+    reply = requests.request("POST",app.config["CHAT"],params=querystring).json()
     return reply
 
-@app.route('/metrics')
-def get_metrics():
-    url = "http://127.0.0.1:5000/metrics"
-
-    querystring_d = {"scope": "day"}
-    querystring_m = {"scope": "month"}
-
-    metrics_day = requests.request("GET", url, params=querystring_d).json()
-    metrics_month = requests.request("GET", url, params=querystring_m).json()
-
-    return 0
 
 @app.route('/timeline')
 def timeline():
-    url = "http://127.0.0.1:5000/metrics"
     querystring_d = {"scope": "day"}
     querystring_m = {"scope": "month"}
-    metrics_day = requests.request("GET", url, params=querystring_d).json()
-    metrics_month = requests.request("GET", url, params=querystring_m).json()
+    metrics_day = requests.request("GET", app.config["METRICS"], params=querystring_d).json()
+    metrics_month = requests.request("GET", app.config["METRICS"], params=querystring_m).json()
 
-    metrics_day_k = list(metrics_day.keys())
-    metrics_day_v = list(metrics_day.values())
-    metrics_month_k = [str(x) for x in list(metrics_month.keys())]
-    metrics_month_v = list(metrics_month.values())
-
-
-
-    return render_template('tabs/timeline.html',metrics_day_k=metrics_day_k,
-                                                                   metrics_day_v=metrics_day_v,
-                           metrics_month_k=metrics_month_k,metrics_month_v=metrics_month_v)
+    return render_template('tabs/timeline.html')
 
 @app.route('/tweets')
 def tweets():
-    t_data = requests.get("http://127.0.0.1:5000/tweetsData").json()
+    t_data = requests.get(app.config["TWEETS_DATA"]).json()
     return render_template('tabs/tweets.html',t_data=t_data)
 
 @app.route('/')
 def index():
     try:
         #timeline graph
-        metrics_d = requests.get("http://127.0.0.1:5000/metrics",params={"scope": "day"}).json()
-        metrics_m = requests.get("http://127.0.0.1:5000/metrics",params={"scope": "month"}).json()
-        metrics_day_k = list(metrics_d.keys())
-        metrics_day_v = list(metrics_d.values())
-        metrics_month_k = list(metrics_m.keys())
-        metrics_month_v = list(metrics_m.values())
-
+        metrics_d = requests.get(app.config["METRICS"],params={"scope": "day"}).json()
+        metrics_m = requests.get(app.config["METRICS"],params={"scope": "month"}).json()
 
         #Most recent tweets
-        t_data = requests.get("http://127.0.0.1:5000/tweets").json()
+        t_data = requests.get(app.config["TWEETS"]).json()
 
         #Sentiment data
-        s_data = requests.get("http://127.0.0.1:5000/polarity").json()
+        s_data = requests.get(app.config["POLARITY"]).json()
 
         #Unique users
-        user_metrics = requests.get("http://127.0.0.1:5000/userMetrics",params={"scope":"day"}).json()
-        user__monthly_metrics = requests.get("http://127.0.0.1:5000/userMetrics",params={"scope":"month"}).json()
+        user_metrics = requests.get(app.config["USER_METRICS"],params={"scope":"day"}).json()
+        user__monthly_metrics = requests.get(app.config["USER_METRICS"],params={"scope":"month"}).json()
 
         #greviences
-        g_data = requests.get("http://127.0.0.1:5000/grev").json()
+        g_data = requests.get(app.config["GREV"]).json()
+
+        #Total data
+        total_tweets = requests.get(app.config["TOTAL_TWEETS"]).json()
 
 
-        total_tweets = requests.get("http://127.0.0.1:5000/totalTweets").json()
-
-
-        return render_template('index.html',t_data=t_data, s_data=s_data,metrics_day_k=metrics_day_k,
-                                                                       metrics_day_v=metrics_day_v,
-                               metrics_month_k=metrics_month_k,metrics_month_v=metrics_month_v,
+        return render_template('index.html',t_data=t_data, s_data=s_data,metrics_d=metrics_d,metrics_m=metrics_m,
                                user_metrics=user_metrics,total_tweets=total_tweets,
                                user__monthly_metrics=user__monthly_metrics,g_data=g_data)
     except Exception as m:
         print(m)
         return render_template('errors/500.html')
 
-#
-# @app.route('/send_message', methods=['POST'])
-# def chatbot_message():
-#     message = str(request.get_data(),'utf-8').split("=")[1]
-#     chat_response = chatbot.chatbot_response(message)
-#     return "Okay cool"
-#
 
 if __name__ == '__main__':
+    app.config.update(
+        TWEETS_DATA="http://127.0.0.1:5000/tweetsData",
+        TOTAL_TWEETS="http://127.0.0.1:5000/totalTweets",
+        GREV = "http://127.0.0.1:5000/grev",
+        USER_METRICS = "http://127.0.0.1:5000/userMetrics",
+        POLARITY = "http://127.0.0.1:5000/polarity",
+        TWEETS = "http://127.0.0.1:5000/tweets",
+        METRICS = "http://127.0.0.1:5000/metrics",
+        CHAT = "http://127.0.0.1:5000/chat"
+    )
     app.run(port=9001)
